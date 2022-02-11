e4c948ec6ba49a892d0621d970119c311e3ba325
SHEN <rgszyxyz@163.com>
2022-02-11 19:20:16 +0800

    Update hosts_ad.py

--- hosts_ad.py
+++ hosts_ad.py
@@ -1,10 +1,11 @@
 import os
+import re
 
 with open("easylistchina.txt",encoding='utf-8') as file:
     for f in file:
         if f[:2]=="||":
-            print (f[:2])
             domain=f.split("^")[0][2:]
-            print (domain)
+            if domain.find("*")<0 and domain.find("@")<0 and domain.find("/")<0 and domain.find("=")<0 and domain.find("$")<0:
+                print ("0.0.0.0 " + domain)
 
 
