5871ef5c7158a6acd3080514cbda776e8eb15db0
SHEN <rgszyxyz@163.com>
2022-02-12 00:36:09 +0800

    Update hosts_ad.py

--- hosts_ad.py
+++ hosts_ad.py
@@ -3,9 +3,13 @@ import re
 
 with open("easylistchina.txt",encoding='utf-8') as file:
     for f in file:
-        if f[:2]=="||":
+        #if f[:2]=="||":
+        if re.match("^\|\|.*\^",f):
+            #print (f)
             domain=f.split("^")[0][2:]
-            if domain.find("*")<0 and domain.find("@")<0 and domain.find("/")<0 and domain.find("=")<0 and domain.find("$")<0:
-                print ("0.0.0.0 " + domain)
+            #print (domain)
+            if f.split("^")[1].find('\n')==0 or f.split("^")[1].find('$third-party')==0:
+            #if domain.find("third-party")>0 and domain.find("@")<0 and domain.find("/")<0 and domain.find("=")<0 and domain.find("$")<0:
+                print ("0.0.0.0 " + domain, "\t"+f, end='')
 
 
